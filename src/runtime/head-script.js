c4afa57cc40cd498ceb84ac625eae60a92e4ed52
Karol Wypchlo <kwypchlo@gmail.com>
2020-03-15 14:51:39 +0100

    customize path regex for skynet

--- src/runtime/head-script.js
+++ src/runtime/head-script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-    var ipfsPathRegExp = /^(\/(?:ipfs|ipns)\/[^/]+)/;
+    var ipfsPathRegExp = /^(\/[a-zA-Z0-9-_]{46})\//;
     var ipfsPathPrefix = (window.location.pathname.match(ipfsPathRegExp) || [])[1] || '';
 
     window.__GATSBY_IPFS_PATH_PREFIX__ = ipfsPathPrefix;
